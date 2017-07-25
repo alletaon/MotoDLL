a43a9a37bbe7a0dcfe56cbd6c4216f166f29a2c7
Aleksander <aksakallon@gmail.com>
2017-07-25 16:06:41 +0300

    replace example usage to new file

--- main.py
+++ main.py
@@ -212,41 +212,4 @@ class MotoDll:
     def __chekResult(self, result):
         if int(result) < 0:
             raise Exception(self.errorCode[int(result)])
-"""
-# EXAMPLE USAGE:
-moto = MotoDll()
-moto.initParser()
-repeatCount = 10
-count = 0
-while repeatCount > 0:
-    count = moto.enumDevices(True)
-    if count > 0:
-        break
-    repeatCount -= 1
-print('Count: ', count)
-if count == 0:
-    print('No devices available')
-    exit(1)
-moto.openPort(1)
-# read params cp1 from drive
-val = moto.readVal(1, b'cp1')
-print('cp1 = ', val)
-moto.writeVal(1, b'cp1', 3.0)
-val_new = moto.readVal(1, b'cp1')
-print('cp1 = ', val_new)
-moto.writeVal(1, b'cp1', val)
-moto.startPLC(1, 3)
-# read current program bank must be 3
-print(moto.readVal(1, b'ip2'))
-sleep(2)
-# read current plc status
-print(moto.readVal(1, b'ip11'))
-moto.stopPLC(1)
-sleep(2)
-# read current program bank must be 255
-print(moto.readVal(1, b'ip2'))
-print(moto.getPortName(1))
-#print(moto.loadProgram(1, 1))
-moto.closePort(1)
-moto.closeParser()
-"""
+
